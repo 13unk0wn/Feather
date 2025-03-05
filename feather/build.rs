0890b3d9a7d45e743db8cb354b5fd302117990cf
Brendan McDonald <hello.brendanm@gmail.com>
2025-03-05 12:17:53 -0500

    update probe to only run on macos

--- feather/build.rs
+++ feather/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    if pkg_config::probe_library("mpv").is_err() {
+    if cfg!(target_os = "macos") && pkg_config::probe_library("mpv").is_err() {
         println!("cargo:warning=Could not find mpv via pkg-config. Make sure it is installed");
     }
 }
